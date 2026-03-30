41cc34ccb8ed03c4ca1a60fad09ca4d2c14491e6
LTLTW\AllenHuang <AllenHuang@worldfamilyenglish.com>
2026-03-30 11:53:52 +0800

    app health

--- src/Demo.Worker/Program.cs
+++ src/Demo.Worker/Program.cs
@@ -4,7 +4,7 @@ using StackExchange.Redis;
 var builder = Host.CreateApplicationBuilder(args);
 
 // 讀取設定檔，如果沒設定就預設 localhost
-var redisConnectionString = builder.Configuration.GetValue<string>("Redis:ConnectionString") ?? "localhost:6379";
+var redisConnectionString = builder.Configuration.GetConnectionString("Redis") ?? "localhost:6379";
 builder.Services.AddSingleton<IConnectionMultiplexer>(sp =>
 {
     // 加上 abortConnect=false，這樣 Redis 暫時斷線時，程式不會直接掛掉
